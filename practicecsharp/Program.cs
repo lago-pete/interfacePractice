eb7a933628eba5515219fedf9d1aa58bccdbda63
lago-pete <lagonegropeter@gmail.com>
2025-04-04 15:22:09 -0700

    first

---
+++ practicecsharp/Program.cs
@@ -0,0 +1,55 @@
+﻿
+public interface ICharacter
+{
+    void Attack();
+    void Defend();
+
+}
+
+public class Knight : ICharacter
+{
+    public void Attack()
+    {
+        Console.WriteLine("The Knight Attacks");
+    }
+    public void Defend()
+    {
+        Console.WriteLine("The Knight defends");
+    }
+}
+
+public class Wizard : ICharacter
+{
+    public void Attack()
+    {
+        Console.WriteLine("The Wizard Attacks");
+    }
+    public void Defend()
+    {
+        Console.WriteLine("The Wizard defends");
+    }
+}
+
+
+public class Game
+{
+    public static void Main()
+    {
+        ICharacter knight = new Knight();
+        ICharacter wizard = new Wizard();
+
+
+        knight.Attack();
+        knight.Defend();
+
+        wizard.Attack();
+        wizard.Defend();
+
+    }
+
+
+
+
+
+
+}
