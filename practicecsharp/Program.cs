7bf1d580a7581a314b5306714bfe96e0405560f2
lago-pete <lagonegropeter@gmail.com>
2025-04-04 19:53:26 -0700

    Unironically Had so much fun reading the out put of this, sounds like an exciting battle between multiple foes

--- practicecsharp/Program.cs
+++ practicecsharp/Program.cs
@@ -1,68 +1,101 @@
 ﻿
 using System.Reflection.PortableExecutable;
+using Microsoft.VisualBasic.FileIO;
 
-public interface ICharacter
+
+
+public abstract class CharacterBase
 {
-    void Attack();
-    void Defend();
+    public string? Name { get; set; }
+
+    public int Health { get; set; } = 100;
+
+    public abstract void Attack(CharacterBase target);
+    public abstract void Defend();
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+    }
+
+    public bool isDead => Health > 0;
+
+
 
 }
+
 
 public interface ISpecialAbility
 {
-
-    void UseSpecial();
+    void UseSpecial(CharacterBase target);
 
 }
 
 
 
-
-
-
-public class Knight : ICharacter
+public class Knight : CharacterBase
 {
-    public void Attack()
+    public Knight() { Name = "Knight"; }
+
+    public override void Attack(CharacterBase target)
     {
-        Console.WriteLine("The Knight Attacks");
+        Console.WriteLine($"{Name} swings a sword at {target.Name}!");
+        target.TakeDamage(15);
     }
-    public void Defend()
+    public override void Defend()
     {
-        Console.WriteLine("The Knight defends");
+        Console.WriteLine($"{Name} raises a shield!");
     }
+
 }
 
-public class Wizard : ICharacter, ISpecialAbility
+public class Wizard : CharacterBase, ISpecialAbility
 {
-    public void Attack()
+    public Wizard() { Name = "Wizard"; }
+
+    public override void Attack(CharacterBase target)
     {
-        Console.WriteLine("The Wizard Attacks");
+        Console.WriteLine($"{Name} swings a sword at {target.Name}!");
+        target.TakeDamage(15);
     }
-    public void Defend()
+
+    public override void Defend()
     {
-        Console.WriteLine("The Wizard defends");
+        Console.WriteLine($"{Name} raises a shield!");
     }
-    public void UseSpecial()
+
+    public void UseSpecial(CharacterBase target)
     {
-        Console.WriteLine("The Wizard Now uses It's special ability");
+        Console.WriteLine($"{Name} used they're special move! Striking {target.Name}");
+        target.TakeDamage(48);
     }
+
 }
 
-public class Mage : ICharacter, ISpecialAbility
+public class Mage : CharacterBase, ISpecialAbility
 {
-    public void Attack()
+    public Mage() { Name = "Mage"; }
+
+    public override void Attack(CharacterBase target)
     {
-        Console.WriteLine("The Mage Attacks");
+        Console.WriteLine($"{Name} swings a sword at {target.Name}!");
+        target.TakeDamage(15);
     }
-    public void Defend()
+
+    public override void Defend()
     {
-        Console.WriteLine("The Mage defends");
+        Console.WriteLine($"{Name} raises a shield!");
     }
-    public void UseSpecial()
+
+    public void UseSpecial(CharacterBase target)
     {
-        Console.WriteLine("The Mage Now uses It's special ability");
+        Console.WriteLine($"{Name} used they're special move! Striking {target.Name}");
+        target.TakeDamage(48);
     }
+
 }
+
+
 
 
 
@@ -73,26 +106,132 @@ public class Game
 {
     public static void Main()
     {
-        ICharacter knight = new Knight();
+        Random random = new Random();
+        int number_of_turn = 100;
+        Knight knight = new Knight();
         Mage mage = new Mage();
         Wizard wizard = new Wizard();
 
-        List<ICharacter> characters = new List<ICharacter>();
+        List<CharacterBase> characters = new List<CharacterBase>();
         characters.Add(knight);
         characters.Add(mage);
         characters.Add(wizard);
 
+        
+        Console.WriteLine("\n\nFirst to Die!");
+        Console.WriteLine("Battle Between Wizard, Knight, and the Mage!\n");
 
-        foreach (ICharacter character in characters)
+
+        for (int i = 0; i < number_of_turn; i++)
         {
-            character.Attack();
-            character.Defend();
 
-            if (character is ISpecialAbility special)
+            Console.WriteLine("\n");
+            Console.WriteLine($"Turn: {i + 1} \n");
+            int rand_player1 = random.Next(0, 3);
+            int rand_player2 = random.Next(0, 3);
+            int special_rand = random.Next(0, 2); //yes or no for the special power
+
+            int player1_a_or_d = random.Next(0, 2);  // 0 = attack and 1 = Defence
+            int player2_a_or_d = random.Next(0, 2);
+
+
+            while (rand_player1 == rand_player2)
             {
-                special.UseSpecial();
+                rand_player2 = random.Next(0, 3);
             }
-            Console.WriteLine("");
+            CharacterBase player1 = characters[rand_player1];
+            CharacterBase player2 = characters[rand_player2];
+
+            if (player1_a_or_d == 1 && player2_a_or_d == 1)
+            {
+                player1.Defend();
+                player2.Defend();
+                Console.WriteLine($"{player1.Name} Went to Block! However so Did {player2.Name}\n ThereFore Sheilds Collided, No Damage Dealt Either Way!\n");
+
+                Console.WriteLine($"{player1.Name}'s Health is : {player1.Health} and {player2.Name}'s Health is : {player2.Health}");
+                continue;
+            }
+
+
+
+            if (player1_a_or_d == 0) // attack
+            {
+                if (player1 is ISpecialAbility special && special_rand == 1)
+                {
+                    if (player2_a_or_d == 1) // but player 2 blocks
+                    {
+                        player2.Defend();
+                        Console.WriteLine($"{player1.Name} Used They're Special Ability! However {player2.Name} Used their Defence! No Damage was Taken!\n");
+                    }
+                    else
+                    {
+                        special.UseSpecial(player2);
+                    }
+
+                }
+                else if (player2_a_or_d == 1) // but player 2 blocks
+                {
+                    player2.Defend();
+                    Console.WriteLine($"{player1.Name} Used They're Melee! However {player2.Name} Used their Defence! No Damage was Taken!\n");
+                }
+                else
+                {
+                    player1.Attack(player2);
+                }
+
+
+            }
+
+            if (player2_a_or_d == 0) // attack
+            {
+                if (player2 is ISpecialAbility special && special_rand == 1)
+                {
+                    if (player1_a_or_d == 1) // but player 2 blocks
+                    {
+                        player1.Defend();
+                        Console.WriteLine($"{player2.Name} Used They're Special Ability! However {player1.Name} Used their Defence! No Damage was Taken!\n");
+                    }
+                    else
+                    {
+                        special.UseSpecial(player1);
+                    }
+
+                }
+
+                else if (player1_a_or_d == 1) // but player 2 blocks
+                {
+                    player1.Defend();
+                    Console.WriteLine($"{player2.Name} Used They're Melee! However {player1.Name} Used their Defence! No Damage was Taken!\n");
+                }
+                else
+                {
+                    player2.Attack(player1);
+                }
+
+            }
+
+            if (!player1.isDead)
+            {
+                Console.WriteLine($"{player1.Name} Has Fallen {player2.Name} Is Victorious!");
+                break;
+            }
+            if (!player2.isDead)
+            {
+                Console.WriteLine($"{player2.Name} Has Fallen {player1.Name} Is Victorious!");
+                break;
+            }
+        
+
+
+
+
+            Console.WriteLine($"{player1.Name}'s Health is : {player1.Health} and {player2.Name}'s Health is : {player2.Health}");
+
+
+
+
+
+
 
         }
 
@@ -110,4 +249,12 @@ public class Game
 
 
 
+
+
+
 }
+
+
+
+
+
